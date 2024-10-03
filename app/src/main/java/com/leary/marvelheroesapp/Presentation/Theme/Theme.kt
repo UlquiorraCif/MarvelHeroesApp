0d5c33c47d1865fd3c5a8302f2eb38b3471243f3
UlquiorraCif <93728068+UlquiorraCif@users.noreply.github.com>
2024-10-03 15:33:13 +0600

    Unused colors removed

--- app/src/main/java/com/leary/marvelheroesapp/Presentation/Theme/Theme.kt
+++ app/src/main/java/com/leary/marvelheroesapp/Presentation/Theme/Theme.kt
@@ -18,18 +18,14 @@ import androidx.core.view.WindowCompat
 
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+
     background = Gray,
     onSecondary = White,
     onBackground = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+
     background = Gray,
     onSecondary = White,
     onBackground = Black
